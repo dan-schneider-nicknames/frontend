d2b6ef56665b41ad03bac304c31ccfd9935b491a
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-11-30 20:24:40 -0600

    added page templates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Switch, Route } from "react-router-dom"
 import PrivateRoute from "./components/common/PrivateRoute";
+import Login from "./components/pages/Login"
+import Signup from "./components/pages/Signup"
+import Nicknames from "./components/pages/Nicknames"
 
 function App() {
   return (
     <Switch>
-      <Route path="/signup" />
-      <Route path="/login" />
-      <PrivateRoute path="/nicknames" />
+      <Route path="/signup">
+        <Login/>
+      </Route>
+      <Route path="/login">
+        <Signup/>
+      </Route>
+      <PrivateRoute path="/nicknames">
+        <Nicknames/>
+      </PrivateRoute>
     </Switch>
   );
 }
