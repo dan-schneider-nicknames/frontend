e0ba01c1405d39cf15426b452b07e4c9208aced9
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-02 11:54:45 -0600

    added form to signup page and boiler plate for yup schemas

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Form from "../common/Form"
+
+const initialState = {
+    email: "",
+    username: "",
+    password: "",
+}
 
 export default function Signup() {
+    const submit = () => {}
+    
     return (
         <div>
-            
+            <Form 
+                initialState={initialState} 
+                submit={submit}
+            />
         </div>
     )
 }
