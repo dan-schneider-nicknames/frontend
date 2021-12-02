1c4240761633d0c76615c8b8aaa0ad28f27b5699
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-02 12:08:37 -0600

    built yup schemas

---
+++ src/schemas/users.js
@@ -0,0 +1,20 @@
+import * as yup from "yup"
+
+const shared = {
+    username: yup.string().required().trim().min(6),
+    password: yup.string().required().trim().min(6)
+}
+
+const loginSchema = yup.object().shape({
+    ...shared
+})
+
+const signupSchema = yup.object().shape({
+    ...shared,
+    email: yup.string().email().trim(),
+})
+
+export default {
+    loginSchema,
+    signupSchema
+}
