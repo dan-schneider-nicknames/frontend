147f5e976e347d1164d909cf014fa37fc1d22e1f
Bradford-Meziere <coleokc@gmail.com>
2021-12-10 20:14:54 -0600

    Fixed Crashing with invalid credentials on signup & login forms.

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,43 +1,45 @@
-import React, { useEffect } from 'react'
-import Form from '../common/Form'
-import { loginSchema } from '../../schemas/users'
-import { useMutation } from '@apollo/client'
-import { LOGIN_CALL } from '../../gqlStatements/mutations'
-import { useNavigate } from 'react-router'
-import { connect } from 'react-redux'
-import { setToken } from "../../state/actions"
+import React, { useEffect } from "react";
+import Form from "../common/Form";
+import { loginSchema } from "../../schemas/users";
+import { useMutation } from "@apollo/client";
+import { LOGIN_CALL } from "../../gqlStatements/mutations";
+import { useNavigate } from "react-router";
+import { connect } from "react-redux";
+import { setToken } from "../../state/actions";
 
 const initialState = {
-    username: "",
-    password: ""
-}
+  username: "",
+  password: "",
+};
 
 function Login(props) {
-    const { setToken } = props
+  const { setToken } = props;
 
-    const navigate = useNavigate()
-    const [loginCall, { data }] = useMutation(LOGIN_CALL)
+  const navigate = useNavigate();
+  const [loginCall, { data, error, loading }] = useMutation(LOGIN_CALL, {
+    errorPolicy: "all",
+  });
 
-    const submit = form => loginCall({ variables: form })
+  const submit = (form) => {
+    loginCall({ variables: form });
+    console.log("Login Form", form);
+  };
 
-    useEffect(() => {
-        if (data) {
-            setToken(data.login)
-            navigate("/")
-        }
-    }, [data]) // eslint-disable-line
+  useEffect(() => {
+    if (data && !error) {
+      setToken(data.login);
+      navigate("/");
+    }
+  }, [data]); // eslint-disable-line
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <Form 
-                initialState={initialState}
-                schema={loginSchema}
-                submit={submit}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <h2>Login</h2>
+      <Form initialState={initialState} schema={loginSchema} submit={submit} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Invalid Credentials</p>}
+    </div>
+  );
 }
 
-
-export default connect(null, { setToken })(Login)
+export default connect(null, { setToken })(Login);
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,43 +1,42 @@
-import React, { useEffect } from 'react'
-import Form from "../common/Form"
-import { signupSchema } from "../../schemas/users"
-import { SIGNUP } from '../../gqlStatements/mutations'
-import { useMutation } from '@apollo/client'
-import { useNavigate } from 'react-router'
-import { connect } from 'react-redux'
-import { setToken} from "../../state/actions"
+import React, { useEffect } from "react";
+import Form from "../common/Form";
+import { signupSchema } from "../../schemas/users";
+import { SIGNUP } from "../../gqlStatements/mutations";
+import { useMutation } from "@apollo/client";
+import { useNavigate } from "react-router";
+import { connect } from "react-redux";
+import { setToken } from "../../state/actions";
 
 const initialState = {
-    email: "",
-    username: "",
-    password: "",
-}
+  email: "",
+  username: "",
+  password: "",
+};
 
 function Signup(props) {
-    const { setToken } = props
-    const navigate = useNavigate()
-    const [signup, { data }] = useMutation(SIGNUP)
+  const { setToken } = props;
+  const navigate = useNavigate();
+  const [signup, { data, loading, error }] = useMutation(SIGNUP, {
+    errorPolicy: "all",
+  });
 
-    const submit = form => signup({ variables: form })
+  const submit = (form) => signup({ variables: form });
 
-    useEffect(() => {
-        if (data) {
-            setToken(data.addUser)
-            navigate("/")
-        }
-    }, [data]) // eslint-disable-line
+  useEffect(() => {
+    if (data && !error) {
+      setToken(data.addUser);
+      navigate("/");
+    }
+  }, [data]); // eslint-disable-line
 
-    
-    return (
-        <div>
-            <h2>Sign Up:</h2>
-            <Form 
-                initialState={initialState} 
-                schema={signupSchema}
-                submit={submit}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <h2>Sign Up:</h2>
+      <Form initialState={initialState} schema={signupSchema} submit={submit} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Username & Email must be unique</p>}
+    </div>
+  );
 }
 
-export default connect(null, { setToken })(Signup)
+export default connect(null, { setToken })(Signup);
