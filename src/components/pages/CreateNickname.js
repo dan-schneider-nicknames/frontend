d64471b6b1c7040de3e13ca0a887e16e4c387d89
Bradford-Meziere <coleokc@gmail.com>
2021-12-14 18:02:17 -0600

    AddNickname routes to users nicknames

--- src/components/pages/CreateNickname.js
+++ src/components/pages/CreateNickname.js
@@ -1,41 +1,50 @@
-import React, { useMemo, useState } from 'react'
-import Input from "../common/Input"
-import Button from "../common/Button"
-import { ADD_NICKNAME } from '../../services/mutations'
-import { useMutation } from '@apollo/client'
-
+import React, { useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router";
+import Input from "../common/Input";
+import Button from "../common/Button";
+import { ADD_NICKNAME } from "../../services/mutations";
+import { useMutation } from "@apollo/client";
 
 export default function CreateNickname() {
-    const [nickname, setNickname] = useState("")
-    const [makeNickname, {loading, error, data}] = useMutation(ADD_NICKNAME, {errorPolicy: "all"})
+  const [nickname, setNickname] = useState("");
+  const [makeNickname, { loading, error, data }] = useMutation(ADD_NICKNAME, {
+    errorPolicy: "all",
+  });
 
-    const submit = e => {
-        e.preventDefault()
-        makeNickname({variables: {nickname}})
+  const navigate = useNavigate();
+
+  const submit = (e) => {
+    e.preventDefault();
+    makeNickname({ variables: { nickname } });
+  };
+
+  useEffect(() => {
+    if (data && !error) {
+      const { username } = data.addNickname.user;
+      navigate(`/user/${username}`);
     }
-    
+  }, [data]);
 
-    const handleChange = e => {
-        setNickname(e.target.value)
-    }
-    const disabled = useMemo(() => 
-        !nickname.includes("Dan") && !nickname.includes("Schneider")
-    , [nickname])
+  const handleChange = (e) => {
+    setNickname(e.target.value);
+  };
+  const disabled = useMemo(
+    () => !nickname.includes("Dan") && !nickname.includes("Schneider"),
+    [nickname]
+  );
 
-    return (
-        <div>
-            <h2>Create a new Nickname</h2>
-            <form onSubmit={submit}>
-                <Input 
-                    name="nickname" 
-                    value={nickname}
-                    handleChange={handleChange}
-                />
-                {disabled && <p>Must contain 'Dan' or 'Schneider'</p>}
-                <Button type="submit" disabled={disabled}>
-                    Submit
-                </Button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Create a new Nickname</h2>
+      <form onSubmit={submit}>
+        <Input name="nickname" value={nickname} handleChange={handleChange} />
+        {disabled && <p>Must contain 'Dan' or 'Schneider'</p>}
+        {loading && <p>Loading...</p>}
+        {error && <p>{error.message}</p>}
+        <Button type="submit" disabled={disabled}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
