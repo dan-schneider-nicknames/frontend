a9356eca58dc943153d563ffa3a11c59cef07787
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-15 19:27:31 -0600

    added query call for user nicknames

--- src/components/common/NicknameList.js
+++ src/components/common/NicknameList.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Nickname from './Nickname'
 
 export default function NicknameList(props) {
-    const { nicknames } = props.data
+    const { nicknames, user } = props.data
     return (
         <ul>
-        {nicknames.map((nickname) => (
-            <Nickname {...nickname} key={nickname.nickname_id} />
-        ))}
+            {nicknames?.map((nickname) => (
+                <Nickname {...nickname} key={nickname.nickname_id} />
+            ))}
+            {user?.nicknames?.map((nickname) => (
+                <Nickname {...nickname} key={nickname.nickname_id} />
+            ))}
       </ul>
     )
 }
--- src/components/pages/UserNicknames.js
+++ src/components/pages/UserNicknames.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import { GET_USER_NICKNAMES } from '../../services/queries'
-import options from '../../services/options'
-import { useQuery } from '@apollo/client'
-import Loader from '../common/Loader'
-import Nickname from '../common/Nickname'
+import QueryCall from '../common/QueryCall'
+import NicknameList from '../common/NicknameList'
 
 export default function UserNicknames() {
     const { username } = useParams()
-    const { data, loading, error } = useQuery(GET_USER_NICKNAMES, { 
-        ...options,
-        variables: { username } 
-    })
-
-    if (loading) return <Loader/>
-    if (error) return <p>{error.message}</p>
     
     return (
         <div>
             <h2>{username}'s Nicknames</h2>
-            {
-                data.user.nicknames.map(nickname => 
-                    <Nickname 
-                        key={nickname.nickname_id} 
-                        {...nickname}
-                    />
-                )
-            }
+            <QueryCall 
+                Component={NicknameList}
+                query={GET_USER_NICKNAMES}
+                variables={{ username }}
+            />
         </div>
     )
 }
