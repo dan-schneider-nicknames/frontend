f247917e0e4887e251537eda7a675957cbec56bf
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-17 15:27:10 -0600

    Updates

---
+++ precache-manifest.49d183918bf71508fed819e581350c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eee06ffa745056043ebea17d467f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ddfe642af0e84597303",
+    "url": "/static/css/main.0a5d810b.chunk.css"
+  },
+  {
+    "revision": "e7136be290e4d94c054e",
+    "url": "/static/js/2.fe146589.chunk.js"
+  },
+  {
+    "revision": "a671ed5d6df9640e1f9d1f7f6767d3d3",
+    "url": "/static/js/2.fe146589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddfe642af0e84597303",
+    "url": "/static/js/main.23384a06.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
