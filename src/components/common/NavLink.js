0ff0c4c3275302bdacdd80a93665d987c2200893
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-17 15:51:59 -0600

    fixed nav link

--- src/components/common/NavLink.js
+++ src/components/common/NavLink.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 
 export default function NavLink(props) {
     return (
-        <div className="button button-6">
+        <Link {...props} className="button button-6">
             <div className="spin"></div>
             <Link {...props}>{props.children}</Link>
-        </div>
+        </Link>
     )
 }
