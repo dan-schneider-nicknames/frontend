ea74b849833c93e29aa6381c6af060da38bce1ce
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-22 20:07:55 -0600

    added pic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import theme from "./styles/theme"
 
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
