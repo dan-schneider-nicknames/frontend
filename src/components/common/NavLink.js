3a4a2868c4780f80285a8f2186c77b8fe6440b72
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2022-01-06 22:05:32 -0600

    added persistent hover for nav links

--- src/components/common/NavLink.js
+++ src/components/common/NavLink.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink as Link } from 'react-router-dom'
 
 
 export default function NavLink(props) {
     return (
-        <Link {...props} className="button button-6">
+        <Link {...props} className={({ isActive }) => `button button-6 ${isActive && "hover"}`}>
             <div className="spin"></div>
             <span>{props.children}</span>
         </Link>
